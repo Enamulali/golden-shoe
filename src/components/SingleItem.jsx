46e03058a52d7d120837d93c06fb861db1c9b7b7
Enamul Ali <ea@codeandcreate.co.uk>
2022-09-06 13:02:32 +0100

    products

---
+++ src/components/SingleItem.jsx
@@ -0,0 +1,36 @@
+import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { getItemById } from "../utils/API";
+
+const SingleItem = () => {
+  const { item_id } = useParams();
+
+  const [singleItem, setSingleItem] = useState([]);
+
+  useEffect(() => {
+    getItemById(item_id).then((singleItemFromApi) => {
+      setSingleItem(singleItemFromApi);
+    });
+  }, [item_id]);
+
+  return (
+    <>
+      <div className="single-item">
+        <h3>{singleItem.item_name}</h3>
+        <img
+          src={singleItem.img_url}
+          alt={singleItem.item_name}
+          className="item-img"
+        />
+        <h3>
+          <span>£</span>
+          <span>{singleItem.price}</span>
+        </h3>
+        <p>Description: {singleItem.description}</p>
+        <button>Add to Basket</button>
+      </div>
+    </>
+  );
+};
+
+export default SingleItem;
