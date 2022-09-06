f7ccc966c6f9fdd369827fc488e3e6490522c130
Enamulali <enamul97@icloud.com>
2022-09-06 13:06:38 +0100

    add ios and android

---
+++ android/app/src/main/java/com/golden/shoe/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.golden.shoe.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.golden.shoe.app',
+  appName: 'golden-shoe',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
